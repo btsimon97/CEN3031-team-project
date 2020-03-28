d8bb3f378781d4251c4a911884cac2ff486ee887
btsimon97 <14293218+btsimon97@users.noreply.github.com>
2020-03-28 14:14:45 -0400

    More navbar styling fixup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import { Link } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap'
@@ -9,35 +9,32 @@ import Container from 'react-bootstrap/Container'
 
 const NavBar = (props) => {
     return (
-        <div>
-
-            {/* Page Links */}
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#">Medical Image Search</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <LinkContainer to='/Home'><Nav.Link href="#">Home</Nav.Link></LinkContainer>
-                    <LinkContainer to='/add'><Nav.Link href="#">Add</Nav.Link></LinkContainer>
-                    {props.currentUser ?
-                    (
-                        <NavDropdown title="Account" id="basic-nav-dropdown">
-                        <LinkContainer to='/dashboard'><NavDropdown.Item href="#">Profile</NavDropdown.Item></LinkContainer>
-                        <LinkContainer to='/logout'><NavDropdown.Item href="#">Logout</NavDropdown.Item></LinkContainer>
-                        </NavDropdown>
-                    ) :
-                    ( 
-                        <NavDropdown title="Login/Register" id="basic-nav-dropdown">
-                        <LinkContainer to='/login'><NavDropdown.Item href="#">Login</NavDropdown.Item></LinkContainer>
-                        <LinkContainer to='/signup'><NavDropdown.Item href="#">Sign-Up</NavDropdown.Item></LinkContainer>
-                        </NavDropdown>
-                    )
-                }
-                </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-
-        </div>
+            <Fragment>
+                <Navbar expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand href="#">Medical Image Search</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <LinkContainer to='/Home'><Nav.Link href="#">Home</Nav.Link></LinkContainer>
+                        <LinkContainer to='/add'><Nav.Link href="#">Add</Nav.Link></LinkContainer>
+                        {props.currentUser ?
+                        (
+                            <NavDropdown title="Account" id="basic-nav-dropdown">
+                            <LinkContainer to='/dashboard'><NavDropdown.Item href="#">Profile</NavDropdown.Item></LinkContainer>
+                            <LinkContainer to='/logout'><NavDropdown.Item href="#">Logout</NavDropdown.Item></LinkContainer>
+                            </NavDropdown>
+                        ) :
+                        ( 
+                            <NavDropdown title="Login/Register" id="basic-nav-dropdown">
+                            <LinkContainer to='/login'><NavDropdown.Item href="#">Login</NavDropdown.Item></LinkContainer>
+                            <LinkContainer to='/signup'><NavDropdown.Item href="#">Sign-Up</NavDropdown.Item></LinkContainer>
+                            </NavDropdown>
+                        )
+                    }
+                    </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Fragment>
     )
 };
 
