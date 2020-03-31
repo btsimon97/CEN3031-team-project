d08f919fc1e9052019a7f263813eacdf898b7b3a
Antonio Antonian <aatonian@ufl.edu>
2020-03-31 09:52:34 -0400

    deploy

--- server.js
+++ server.js
@@ -29,43 +29,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
-app.use(cors())
+// app.use(cors())
 
 //enable request logging for development debugging
 app.use(morgan('dev'));
 
-app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-   res.header(
-   "Access-Control-Allow-Headers",
-   "Origin, X-Requested-With, Content-Type, Accept"
-   );
-   next();
-   });
-
-app.options("*", cors());
-
-// const upload = multer({ storage: storage });
-	
-//https://medium.com/@colinrlly/send-store-and-show-images-with-react-express-and-mongodb-592bc38a9ed
-
-// const storage = multer.diskStorage({
-//    destination: function (req, res, cb) {
-//        cb(null, 'uploads/')
-//    }
-// });
-
-// Router.route('/img_data')
-// .post(upload.single('file'), function(req, res) {
-//     var new_img = new img;
-//     new_img.img.data = fs.readFileSync(req.file.path)
-//     new_img.img.contentType = 'image/jpeg';
-//     new_img.save();
-//     res.json({ message: 'New image added to the db!' });
-// })
-
-//body parsing middleware
+// app.use(function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//    res.header(
+//    "Access-Control-Allow-Headers",
+//    "Origin, X-Requested-With, Content-Type, Accept"
+//    );
+//    next();
+//    });
 
 
 if(process.env.NODE_ENV === 'production')
@@ -73,7 +50,7 @@ if(process.env.NODE_ENV === 'production')
    app.use(express.static('build'));
 
    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'))
+      res.sendFile(path.resolve('client','build', 'index.html'))
    })
 }
 // app.use('/', express.static('./../../client'));
