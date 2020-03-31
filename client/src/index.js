222d5cc142f99360c2092c978614610231f059c2
Antonio Antonian <aatonian@ufl.edu>
2020-03-31 00:01:08 -0400

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-//The app is now passing the data as a property
 ReactDOM.render(<App/>, document.getElementById('root'));
