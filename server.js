1aa2f996deedee611adf5eb9f9901a4f8a8aa5b9
Antonio Antonian <aatonian@ufl.edu>
2020-03-31 10:50:10 -0400

    deploy

--- server.js
+++ server.js
@@ -20,40 +20,25 @@ mongoose.connect(config.db.uri, {useNewUrlParser: true}, (error) => {
    }
 })
 
-
 //initialize app
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(bodyParser.json());
-
-// app.use(cors())
-
-//enable request logging for development debugging
 app.use(morgan('dev'));
 
-// app.use(function(req, res, next) {
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//    res.header(
-//    "Access-Control-Allow-Headers",
-//    "Origin, X-Requested-With, Content-Type, Accept"
-//    );
-//    next();
-//    });
 
-
-if(process.env.NODE_ENV === 'production')
+if(process.env.NODE_ENV === 'production') //for deployment
 {
-   app.use(express.static('build'));
+   app.use(express.static(path.join(__dirname, "client", "build")))
 
-   app.get('*', (req, res) => {
-      res.sendFile(path.resolve('client','build', 'index.html'))
-   })
+   app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+  
 }
-// app.use('/', express.static('./../../client'));
+app.use('/', express.static('./../../client'));
 
 app.use('/api/listings/', listingsRouter);
 
