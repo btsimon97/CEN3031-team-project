6a1e81c5db4b2f9cd7b29493e2b8af053fa4ba09
Antonio Antonian <aatonian@ufl.edu>
2020-03-31 00:28:52 -0400

    deploy

--- server.js
+++ server.js
@@ -84,9 +84,9 @@ if(process.env.NODE_ENV === 'production')
 {
    app.use(express.static('client/build'));
 
-   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.hmtl'))
-   })
+   // app.get('*', (req, res) => {
+   //    res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.hmtl'))
+   // })
 }
 
 // app.use('/', express.static('./../../client'));
